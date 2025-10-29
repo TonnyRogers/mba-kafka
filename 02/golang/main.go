c9a73507f66a75af32aa39c8af68a5993bd7d91f
TonnyRogers <antoniel15975@gmail.com>
2025-10-28 22:13:45 -0300

    commit classes

---
+++ 02/golang/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+	// (1) Configuração e Criação do Produtor
+	config := &kafka.ConfigMap{"bootstrap.servers": "localhost"}
+	producer, err := kafka.NewProducer(config)
+	if err != nil {
+		panic(err)
+	}
+	defer producer.Close()
+	// (1) FIM
+
+	// (2) Produção da Mensagem
+	topic := "test"
+	msg := &kafka.Message{
+		Value: []byte("hi"),
+		Key:   []byte("1"),
+		Headers: []kafka.Header{
+			{Key: "regiao", Value: []byte("SP")},
+		},
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+	}
+
+	wait := make(chan kafka.Event)
+	err = producer.Produce(msg, wait)
+	if err != nil {
+		panic(err)
+	}
+	// (2) FIM
+
+	<-wait
+}
